dc7b24d47c872ba6dc18ad8d70029eff6e0f1a3a
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-21 16:06:57 -0400

    Move Button and Link examples into dedicated HTML files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-window.customElements.define('my-button', window.__COMPONENTS__.Button);
-window.customElements.define('my-link', window.__COMPONENTS__.Link);
-window.customElements.define('my-custom-link', window.__COMPONENTS__.CustomLink);
+if (window.__COMPONENTS__.Button) window.customElements.define('my-button', window.__COMPONENTS__.Button);
+if (window.__COMPONENTS__.Link) window.customElements.define('my-link', window.__COMPONENTS__.Link);
+if (window.__COMPONENTS__.CustomLink) window.customElements.define('my-custom-link', window.__COMPONENTS__.CustomLink);
 
 (() => {
   window.addEventListener('DOMContentLoaded', () => {
